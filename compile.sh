ad8d9c5bb2bdc4b49609fe4d1f9cfe35545167c2
Andrew Brownbill <andrew.brownbill@gmail.com>
2019-03-15 09:52:07 -0400

    Turn on -O2 for better performance.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p docs
-emcc game_of_life.cpp --shell-file $EMSCRIPTEN/src/shell_minimal.html -std=c++11 -s WASM=1  -o docs/index.html
+emcc game_of_life.cpp -O2 --shell-file $EMSCRIPTEN/src/shell_minimal.html -std=c++11 -s WASM=1  -o docs/index.html
 
