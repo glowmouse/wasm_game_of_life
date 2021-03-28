0c5b3ff36ffc38a72adfb7845c059973b9ed0c11
andrew <andrew.brownbill@gmail.com>
2021-03-28 09:34:35 -0400

    Create a cmake file for the project

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p docs
-emcc game_of_life.cpp -O2 --shell-file $EMSDK/upstream/emscripten/src/shell_minimal.html -std=c++11 -s WASM=1  -o docs/index.html
+emcc game_of_life.cpp -O2 --shell-file $EMSDK/upstream/emscripten/src/shell_minimal.html -std=c++11 -s WASM=1 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_TTF=2 -o docs/index.html
 
